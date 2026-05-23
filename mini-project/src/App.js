3f15f795b0c146bc2523e869b3f37840f92b9a23
Sung-Jaewoo <jaewoosung0407@gmail.com>
2026-05-24 04:13:09 +0900

    3차 중간저장

--- mini-project/src/App.js
+++ mini-project/src/App.js
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MyPage from "./pages/MyPage";
 import ResetPasswordPage from "./pages/ResetPasswordPage";
 
 function App() {
-  return <ResetPasswordPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MyPage />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/password-reset" element={<ResetPasswordPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
